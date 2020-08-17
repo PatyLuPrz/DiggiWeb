49fe6c1b8fbedb4286a7f611a48cd3fd80ed4ad6
Patricia Perez <1717110752@utectulancingo.edu.mx>
2020-08-17 01:05:01 -0500

    Inicio de sesion arregado

--- application/models/model_success.py
+++ application/models/model_success.py
@@ -9,14 +9,12 @@ def getRestaurante(email):
     try:
         restruante_ref = db.collection(u'restaurantes')
         docs = restruante_ref.stream()
-        for item in docs:
+        print(email)
+        print("####")
+        for item in docs:            
             if item.get('email') == email:
-                print("Id del restaurante")
-                print(item.id)
-                return item.id
-            else:
-                print("Email no localizado - Restaurante")
-                return None
+                clave = item.id
+        return clave
     except Exception as e:
         print( "Error restaurantes: " +str(e.args))
         return None
@@ -25,14 +23,10 @@ def getLocal(email):
     try:
         local_ref = db.collection(u'locales')
         docs = local_ref.stream()
-        for item in docs:
+        for item in docs:            
             if item.get('email') == email:
-                print("Id del local")
-                print(item.id)
-                return item.id
-            else:
-                print("Email no localizado - Locales")
-                return None
+                clave = item.id
+        return clave
     except Exception as e:
         print( "Error locales: " +str(e.args))
         return None
@@ -41,14 +35,10 @@ def getUsuario(email):
     try:
         user_ref = db.collection(u'clientes')
         docs = user_ref.stream()
-        for item in docs:
+        for item in docs:            
             if item.get('email') == email:
-                print("Id del cliente")
-                print(item.id)
-                return item.id
-            else:
-                print("Email no localizado - Clientes")
-                return None
+                clave = item.id
+        return clave
     except Exception as e:
         print( "Error usuarios: " +str(e.args))
         return None
